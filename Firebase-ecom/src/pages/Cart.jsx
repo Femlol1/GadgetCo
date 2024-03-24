77d6676678911787574f6cabe731c7b2fa17e555
Femi <osibemekunfemi@gmail.com>
2024-03-24 21:28:10 +0000

    Updated the shop page

--- Firebase-ecom/src/pages/Cart.jsx
+++ Firebase-ecom/src/pages/Cart.jsx
@@ -9,85 +9,85 @@ import { cartActions } from "../redux/slices/cartSlice";
 import "../styles/cart.css";
 
 const Cart = () => {
-  const cartItems = useSelector((state) => state.cart.cartItems);
-  const totalAmount = useSelector((state) => state.cart.totalAmount);
-  return (
-    <Helmet title="Cart">
-      <CommonSection title="Shopping Cart" />
-      <section>
-        <Container>
-          <Row>
-            <Col lg="9">
-              {cartItems.length === 0 ? (
-                <h2 className="fs-4 text-center">Empty Cart</h2>
-              ) : (
-                <table className="table bordered">
-                  <thead>
-                    <tr>
-                      <th>Image</th>
-                      <th>Title</th>
-                      <th>Price</th>
-                      <th>Qty</th>
-                      <th>Delete</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {cartItems.map((item, index) => (
-                      <CartTable item={item} key={index} />
-                    ))}
-                  </tbody>
-                </table>
-              )}
-            </Col>
-            <Col lg="3">
-              <div>
-                <h6 className="d-flex align-items-center justify-content-between">
-                  SubTotal
-                  <span className="fs-4 fw-bold">£{totalAmount}</span>
-                </h6>
-              </div>
-              <p className="fs-6 mt-2">
-                Taxes and shipping will calculate in checkout
-              </p>
-              <div>
-                <button className="buy__btn w-100">
-                  <Link to="/checkout">Checkout</Link>
-                </button>
-                <button className="buy__btn w-100 mt-3">
-                  <Link to="/shop">Continue Shopping</Link>
-                </button>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </Helmet>
-  );
+	const cartItems = useSelector((state) => state.cart.cartItems);
+	const totalAmount = useSelector((state) => state.cart.totalAmount);
+	return (
+		<Helmet title="Cart">
+			<CommonSection title="Shopping Cart" />
+			<section>
+				<Container>
+					<Row>
+						<Col lg="9">
+							{cartItems.length === 0 ? (
+								<h2 className="fs-4 text-center">Empty Cart</h2>
+							) : (
+								<table className="table bordered">
+									<thead>
+										<tr>
+											<th>Image</th>
+											<th>Title</th>
+											<th>Price</th>
+											<th>Qty</th>
+											<th>Delete</th>
+										</tr>
+									</thead>
+									<tbody>
+										{cartItems.map((item, index) => (
+											<CartTable item={item} key={index} />
+										))}
+									</tbody>
+								</table>
+							)}
+						</Col>
+						<Col lg="3">
+							<div>
+								<h6 className="d-flex align-items-center justify-content-between">
+									SubTotal
+									<span className="fs-4 fw-bold">£{totalAmount}</span>
+								</h6>
+							</div>
+							<p className="fs-6 mt-2">
+								Taxes and shipping will calculate in checkout
+							</p>
+							<div>
+								<button className="buy__btn w-100">
+									<Link to="/checkout">Checkout</Link>
+								</button>
+								<button className="buy__btn w-100 mt-3">
+									<Link to="/shop">Continue Shopping</Link>
+								</button>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		</Helmet>
+	);
 };
 
 const CartTable = ({ item }) => {
-  const dispatch = useDispatch();
-  const deleteProduct = () => {
-    dispatch(cartActions.deleteItem(item.id));
-  };
+	const dispatch = useDispatch();
+	const deleteProduct = () => {
+		dispatch(cartActions.deleteItem(item.id));
+	};
 
-  return (
-    <tr>
-      <td>
-        <img src={item.imgUrl} alt="" />
-      </td>
-      <td>{item.productName}</td>
-      <td>£{item.price}</td>
-      <td>{item.quantity}</td>
-      <td>
-        <motion.i
-          onClick={deleteProduct}
-          whileTap={{ scale: 1.2 }}
-          class="ri-delete-bin-line"
-        ></motion.i>
-      </td>
-    </tr>
-  );
+	return (
+		<tr>
+			<td>
+				<img src={item.imgUrl} alt="" />
+			</td>
+			<td>{item.productName}</td>
+			<td>£{item.price}</td>
+			<td>{item.quantity}</td>
+			<td>
+				<motion.i
+					onClick={deleteProduct}
+					whileTap={{ scale: 1.2 }}
+					class="ri-delete-bin-line"
+				></motion.i>
+			</td>
+		</tr>
+	);
 };
 
 export default Cart;
--- Firebase-ecom/src/pages/Shop.jsx
+++ Firebase-ecom/src/pages/Shop.jsx
@@ -1,118 +1,136 @@
 import React, { useState } from "react";
 import { Col, Container, Row } from "reactstrap";
-import products from "../assets/data/products";
 import Helmet from "../components/Helmet/helmet";
 import CommonSection from "../components/UI/CommonSection";
 import ProductsList from "../components/UI/ProductsList";
 import "../styles/shop.css";
 
+import useGetData from "../custom-hooks/useGetData";
+
 const Shop = () => {
-  const [productsData, setProductsData] = useState(products);
+	const { data: products, loading } = useGetData("products");
+	const [productsData, setProductsData] = useState([]);
+	const [trendingProducts, setTrendingProducts] = useState([]);
+	const [bestSalesProducts, setBestSalesProducts] = useState([]);
+	const [mobileProducts, setMobileProducts] = useState([]);
+	const [wirelessProducts, setWirelessProducts] = useState([]);
+	const [popularProducts, setPopularProducts] = useState([]);
 
-  const handleFilter = (e) => {
-    const filterValue = e.target.value;
-    if (filterValue === "sofa") {
-      const filteredProducts = products.filter(
-        (item) => item.category === "sofa"
-      );
+	// Function to show all products
+	const showAllProducts = () => {
+		setProductsData(products);
+	};
 
-      setProductsData(filteredProducts);
-    }
-    if (filterValue === "mobile") {
-      const filteredProducts = products.filter(
-        (item) => item.category === "mobile"
-      );
+	const handleFilter = (e) => {
+		const filterValue = e.target.value;
+		if (filterValue === "sofa") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "sofa"
+			);
 
-      setProductsData(filteredProducts);
-    }
-    if (filterValue === "chair") {
-      const filteredProducts = products.filter(
-        (item) => item.category === "chair"
-      );
+			setProductsData(filteredProducts);
+		}
+		if (filterValue === "mobile") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "mobile"
+			);
 
-      setProductsData(filteredProducts);
-    }
-    if (filterValue === "watch") {
-      const filteredProducts = products.filter(
-        (item) => item.category === "watch"
-      );
+			setProductsData(filteredProducts);
+		}
+		if (filterValue === "chair") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "chair"
+			);
 
-      setProductsData(filteredProducts);
-    }
-    if (filterValue === "wireless") {
-      const filteredProducts = products.filter(
-        (item) => item.category === "wireless"
-      );
+			setProductsData(filteredProducts);
+		}
+		if (filterValue === "watch") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "watch"
+			);
 
-      setProductsData(filteredProducts);
-    }
-  };
+			setProductsData(filteredProducts);
+		}
+		if (filterValue === "wireless") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "wireless"
+			);
 
-  const handleSearch = (e) => {
-    const searchItems = e.target.value;
+			setProductsData(filteredProducts);
+		}
+	};
 
-    const searchedProducts = products.filter((item) =>
-      item.productName.toLowerCase().includes(searchItems.toLowerCase())
-    );
+	const handleSearch = (e) => {
+		const searchItems = e.target.value;
 
-    setProductsData(searchedProducts);
-  };
+		const searchedProducts = products.filter((item) =>
+			item.productName.toLowerCase().includes(searchItems.toLowerCase())
+		);
 
-  return (
-    <Helmet title="Shop">
-      <CommonSection title="Products" />
-      <section>
-        <Container>
-          <Row>
-            <Col lg="3" md="6">
-              <div className="filter__widget">
-                <select onChange={handleFilter}>
-                  <option>Filter By Category</option>
-                  <option value="sofa">Sofa</option>
-                  <option value="mobile">Mobile</option>
-                  <option value="chair">Chair</option>
-                  <option value="watch">Watch</option>
-                  <option value="wireless">Wireless</option>
-                </select>
-              </div>
-            </Col>
-            <Col lg="3" md="6" className="text-end">
-              <div className="filter__widget">
-                <select name="" id="">
-                  <option>Sort By</option>
-                  <option value="ascending">Ascending</option>
-                  <option value="descending">Descending</option>
-                </select>
-              </div>
-            </Col>
-            <Col lg="6" md="12">
-              <div className="search__box">
-                <input
-                  type="text"
-                  placeholder="Search "
-                  onChange={handleSearch}
-                />
-                <span>
-                  <i class="ri-search-line"></i>
-                </span>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      <section>
-        <Container>
-          <Row>
-            {productsData.length === 0 ? (
-              <h1 className="text-center fs-4">The product is Unavailable!</h1>
-            ) : (
-              <ProductsList data={productsData} />
-            )}
-          </Row>
-        </Container>
-      </section>
-    </Helmet>
-  );
+		setProductsData(searchedProducts);
+	};
+
+	return (
+		<Helmet title="Shop">
+			<CommonSection title="Products" />
+			<section>
+				<Container>
+					<Row>
+						<Col lg="3" md="6">
+							<div className="filter__widget">
+								<select onChange={handleFilter}>
+									<option>Filter By Category</option>
+									<option value="sofa">Sofa</option>
+									<option value="mobile">Mobile</option>
+									<option value="chair">Chair</option>
+									<option value="watch">Watch</option>
+									<option value="wireless">Wireless</option>
+								</select>
+								<button
+									onClick={showAllProducts}
+									className="btn btn-primary mt-3"
+								>
+									Show All Products
+								</button>
+							</div>
+						</Col>
+						<Col lg="3" md="6" className="text-end">
+							<div className="filter__widget">
+								<select name="" id="">
+									<option>Sort By</option>
+									<option value="ascending">Ascending</option>
+									<option value="descending">Descending</option>
+								</select>
+							</div>
+						</Col>
+						<Col lg="6" md="12">
+							<div className="search__box">
+								<input
+									type="text"
+									placeholder="Search "
+									onChange={handleSearch}
+								/>
+								<span>
+									<i class="ri-search-line"></i>
+								</span>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+			<section>
+				<Container>
+					<Row>
+						{productsData.length === 0 ? (
+							<h1 className="text-center fs-4">The product is Unavailable!</h1>
+						) : (
+							<ProductsList data={productsData} />
+						)}
+					</Row>
+				</Container>
+			</section>
+		</Helmet>
+	);
 };
 
 export default Shop;
