864e68de860365a88a519a08ab568e4da7f631b2
Femi <osibemekunfemi@gmail.com>
2024-04-10 22:08:53 +0100

    updataing my dirctory

--- ecom-webapp-frontend/src/App.js
+++ ecom-webapp-frontend/src/App.js
@@ -1,25 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navigation from './components/Navigation';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+
 
 function App() {
-  return (
-    <div className="App">
-    <BrowserRouter>
-    <Navigation />
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/signup' element={<Signup/>} />
-      <Route path='*' element={<Home />} />
-    </Routes>
-    </BrowserRouter>
-    </div>
-  );
+  return <Layout/>;
 }
 
 export default App;
